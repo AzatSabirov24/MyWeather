00d7a9fb596ee5df3e0ab3d43b4f83020784723b
Azat_Sabirov <azat240510@gmail.com>
2021-07-23 12:35:08 +0300

    Lesson 5_HW

--- app/src/main/java/com/azat_sabirov/myweather/model/Repository.kt
+++
@@ -1,7 +0,0 @@
-package com.azat_sabirov.myweather.model
-
-interface Repository {
-   fun getWeatherFromServer(): Weather
-   fun getWeatherFromLocalStorageRus(): List<Weather>
-   fun getWeatherFromLocalStorageWorld(): List<Weather>
-}
